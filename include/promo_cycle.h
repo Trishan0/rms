37684ed48e7957293aba7862415b15d9ef4ec342
trishan-crede <sanjanatrishan@gmail.com>
2026-03-23 11:00:44 +0530

    Add new header files for supermarket mgt system and removed the railway mgt sytem header files

---
+++ include/promo_cycle.h
@@ -0,0 +1,12 @@
+#ifndef PROMO_CYCLE_H
+#define PROMO_CYCLE_H
+
+void promoCycleMenu(void);
+void addPromo(void);
+void displayNextPromo(void);
+void searchPromo(void);
+void updatePromo(void);
+void deletePromo(void);
+void countActivePromos(void);
+
+#endif
